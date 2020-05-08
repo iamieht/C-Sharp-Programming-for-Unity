cb20c1c6bf45cc1804d1117d09eec2e2e97e2972
Iván E. Hernández T <iamieht@gmail.com>
2020-05-08 21:20:48 +0200

    Exercise1
    Problem 1: Add a line of code that will display your name to the user.
    Problem 2: Add code to output the name of your best friend or your nemesis.

---
+++ Introduction to C# Programming and Unity/Week1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1: Add a line of code that will display your name to the user.
+            Console.WriteLine("Ivan");
+            Console.WriteLine();
+
+            //Problem 2: Add code to output the name of your best friend or your nemesis.
+            Console.WriteLine("My best friend: LaFlaca");
+            Console.WriteLine();
+        }
+    }
+}
