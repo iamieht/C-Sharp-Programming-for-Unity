836154cc1a26c95abca3756a3ae4d78f99be04c7
Iván E. Hernández T <iamieht@gmail.com>
2020-05-10 19:55:16 +0200

    Exercise6

---
+++ Introduction to C# Programming and Unity/Week1/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter an angle in degrees");
+            float angle = float.Parse(Console.ReadLine());
+
+            angle *= (float)Math.PI / 180;
+            Console.WriteLine("Cosine: " + Math.Cos(angle));
+            Console.WriteLine("Sine:   " + Math.Sin(angle));
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
