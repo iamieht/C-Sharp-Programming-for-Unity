d5ff81b28599fbcf5b0b34787116f1e9f674c078
Iván E. Hernández T <iamieht@gmail.com>
2020-05-10 19:03:56 +0200

    Week1 - Exercise 4

---
+++ Introduction to C# Programming and Unity/Week1/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter first altitude");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter second altitude");
+            int secondAltitude = int.Parse(Console.ReadLine());
+
+            int altitudeChange;
+            altitudeChange = secondAltitude - firstAltitude;
+
+            Console.WriteLine("Altitude Change is " + altitudeChange);
+        }
+    }
+}
