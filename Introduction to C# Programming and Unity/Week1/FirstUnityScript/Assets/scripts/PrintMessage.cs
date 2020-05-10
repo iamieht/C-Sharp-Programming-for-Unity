8f562f21b4197a87e549681eb13ca928bdad8382
Iván E. Hernández T <iamieht@gmail.com>
2020-05-10 19:05:31 +0200

    First Unity Script

---
+++ Introduction to C# Programming and Unity/Week1/FirstUnityScript/Assets/scripts/PrintMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // print supportive message
+        print("Hi, n00b!");
+        
+    }
+
+}
