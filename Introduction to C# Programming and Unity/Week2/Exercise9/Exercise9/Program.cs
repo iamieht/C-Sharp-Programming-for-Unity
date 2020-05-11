e5aedad99c22cb804ca478a42e9dc9889bcef01b
Iván E. Hernández T <iamieht@gmail.com>
2020-05-11 19:45:46 +0200

    Exercise 9: Look at the cards

---
+++ Introduction to C# Programming and Unity/Week2/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{    
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+            Console.WriteLine();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card card1 = deck.TakeTopCard();
+            Console.WriteLine("Card #1 Rank: " + card1.Rank);
+            Console.WriteLine("Card #1 Suit: " + card1.Suit);
+
+
+            // take the top card from the deck and print the card rank and suit
+            Card card2 = deck.TakeTopCard();
+            Console.WriteLine("Card #2 Rank: " + card2.Rank);
+            Console.WriteLine("Card #2 Suit: " + card2.Suit);
+        }
+    }
+}
